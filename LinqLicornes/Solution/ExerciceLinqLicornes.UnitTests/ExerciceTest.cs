ce6fc886074e358f61ac0d6663f4030721b5323b
Sybaris <Sybaris@Club-Internet.Fr>
2021-01-12 10:22:31 +0100

    Added declarative syntax

--- LinqLicornes/Solution/ExerciceLinqLicornes.UnitTests/ExerciceTest.cs
+++ LinqLicornes/Solution/ExerciceLinqLicornes.UnitTests/ExerciceTest.cs
@@ -21,7 +21,11 @@ namespace ExerciceLinqLicornes.UnitTests
         public void GetNomLicorne()
         {
             var res = Exercice.GetNomLicorne();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.Select(x => x.Name);
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               select licorne.Name;
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -30,7 +34,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void TrouverLesLicornesParmiLesAnimaux()
         {
             var res = Exercice.TrouverLesLicornesParmiLesAnimaux();
+            // Version méthode d'extension
             var expected = Exercice.ListeAnimaux.OfType<Licorne>();
+            // Version déclarative
+            //var expected = from animal in Exercice.ListeAnimaux
+            //               where animal is Licorne
+            //               select animal;
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -39,7 +48,9 @@ namespace ExerciceLinqLicornes.UnitTests
         public void LesLicornesSontDesAnimaux()
         {
             IEnumerable<Animal> res = Exercice.LesLicornesSontDesAnimaux();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne;
+            // Pas de version déclarative...
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -48,7 +59,9 @@ namespace ExerciceLinqLicornes.UnitTests
         public void LesAnimauxQuiNontPasLaChanceDEtreDesLicornes()
         {
             IEnumerable<Animal> res = Exercice.LesAnimauxQuiNontPasLaChanceDEtreDesLicornes();
+            // Version méthode d'extension
             var expected = Exercice.ListeAnimaux.Except(Exercice.ListeLicorne);
+            // Pas de version déclarative...
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -57,7 +70,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void LicorneLesPlusJeuneDabord()
         {
             IEnumerable<Animal> res = Exercice.LicorneLesPlusJeuneDabord();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.OrderBy(x => x.Age);
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               orderby licorne.Age
+            //               select licorne;
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -66,7 +84,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void LicorneLesPlusJeuneDabordMaislesAAussi()
         {
             IEnumerable<Animal> res = Exercice.LicorneLesPlusJeuneDabordMaislesAAussi();
-            var expected = Exercice.ListeLicorne.OrderBy(x => x.Age).ThenBy(x=>x.Name);
+            // Version méthode d'extension
+            var expected = Exercice.ListeLicorne.OrderBy(x => x.Age).ThenBy(x => x.Name);
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               orderby licorne.Age, licorne.Name
+            //               select licorne;
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -75,7 +98,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void CeuxQuiOnt10ansLevezLaMain()
         {
             var res = Exercice.CeuxQuiOnt10ansLevezLaMain();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.Where(x => x.Age == 10);
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               where licorne.Age == 10
+            //               select licorne;
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -84,7 +112,9 @@ namespace ExerciceLinqLicornes.UnitTests
         public void LesLicornesOntDuCaractere()
         {
             var res = Exercice.LesLicornesOntDuCaractere();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.SelectMany(x => x.ListePeronnalites);
+            // Pas de version déclarative...
             Assert.IsTrue(res.SequenceEqual(expected));
         }
 
@@ -93,7 +123,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void PyramideDesAgesDesLicornes()
         {
             var res = Exercice.PyramideDesAgesDesLicornes();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.GroupBy(x => x.Age, (x, y) => new Tuple<int, int>(x, y.Count()));
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               group licorne by licorne.Age into newGroup
+            //               select new Tuple<int, int>(newGroup.Key, newGroup.Count());
             Assert.IsTrue(res.Count() == expected.Count());
             Assert.IsTrue(res.Zip(expected, (a, b) => a.Item1 == b.Item1 && a.Item2 == b.Item2).All(x => x));
         }
@@ -103,7 +138,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void QuelleLicorneAMonAge()
         {
             var res = Exercice.QuelleLicorneAMonAge();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.GroupBy(x => x.Age).ToDictionary(x => x.Key, x => x.ToList());
+            // Version déclarative
+            //var expected = (from licorne in Exercice.ListeLicorne
+            //                group licorne by licorne.Age into newGroup
+            //                select newGroup).ToDictionary(x => x.Key, x => x.ToList());
             Assert.IsTrue(res.Count() == expected.Count());
             Assert.IsTrue(res.Zip(expected, (a, b) => a.Key == b.Key && a.Value.SequenceEqual(b.Value)).All(x => x));
         }
@@ -113,7 +153,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void QuelleLicorneAMonAgeLookup()
         {
             var res = Exercice.QuelleLicorneAMonAgeLookup();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.ToLookup(x => x.Age);
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               group licorne by licorne.Age into newGroup
+            //               select newGroup;
             Assert.IsTrue(res.Count() == expected.Count());
             Assert.IsTrue(res.Zip(expected, (a, b) => a.Key == b.Key && a.SequenceEqual(b)).All(x => x));
         }
@@ -123,7 +168,12 @@ namespace ExerciceLinqLicornes.UnitTests
         public void AChacunSonJouet()
         {
             var res = Exercice.AChacunSonJouet();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.GroupJoin(Exercice.ListeJouet, x => x.Name, y => y.NomProprietaire, (x, y) => new Tuple<Licorne, List<Jouet>>(x, y.ToList()));
+            // Version déclarative
+            //var expected = from licorne in Exercice.ListeLicorne
+            //               join jouet in Exercice.ListeJouet on licorne.Name equals jouet.NomProprietaire into jouetsDeLaLicorne
+            //               select new Tuple<Licorne, List<Jouet>>(licorne, jouetsDeLaLicorne.ToList());
             Assert.IsTrue(res.Count() == expected.Count());
             Assert.IsTrue(res.Zip(expected, (a, b) => a.Item1 == b.Item1 && a.Item2.SequenceEqual(b.Item2)).All(x => x));
         }
@@ -133,7 +183,9 @@ namespace ExerciceLinqLicornes.UnitTests
         public void MegaLicorne()
         {
             var res = Exercice.MegaLicorne();
+            // Version méthode d'extension
             var expected = Exercice.ListeLicorne.Aggregate((l1, l2) => Licorne.Fusiiioooon(l1, l2));
+            // Pas de version déclarative...
             Assert.IsTrue(res.Couleur == expected.Couleur);
             Assert.IsTrue(res.ListePeronnalites.SequenceEqual(expected.ListePeronnalites));
         }
@@ -143,11 +195,14 @@ namespace ExerciceLinqLicornes.UnitTests
         public void DesBebesLicornes()
         {
             var res = Exercice.DesBebesLicornes();
-            var expected = Exercice.ListeLicorne.Take(3).Zip(Exercice.ListeLicorne.Skip(3), (l1, l2) => Licorne.FaireUnBebe(l1,l2));
+            // Version méthode d'extension
+            var expected = Exercice.ListeLicorne.Take(3).Zip(Exercice.ListeLicorne.Skip(3), (l1, l2) => Licorne.FaireUnBebe(l1, l2));
+            // Pas de version déclarative...
             Assert.IsTrue(res.Count() == expected.Count());
-            Assert.IsTrue(res.Select(x=>x.Couleur).SequenceEqual(expected.Select(x=>x.Couleur)));
+            Assert.IsTrue(res.Select(x => x.Couleur).SequenceEqual(expected.Select(x => x.Couleur)));
         }
 
-       
+
     }
 }
+
--- LinqLicornes/Solution/ExerciceLinqLicornes/ExerciceLinq.cs
+++ LinqLicornes/Solution/ExerciceLinqLicornes/ExerciceLinq.cs
@@ -25,7 +25,11 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<string> GetNomLicorne()
         {
+            // Version méthode d'extension
             return ListeLicorne.Select(l => l.Name);
+            // Version déclarative
+            //return from licorne in ListeLicorne
+            //       select licorne.Name;
         }
 
         /// <summary>
@@ -34,7 +38,12 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Licorne> TrouverLesLicornesParmiLesAnimaux()
         {
+            // Version méthode d'extension
             return ListeAnimaux.OfType<Licorne>();
+            // Version déclarative
+            //return from animal in ListeAnimaux
+            //       where animal is Licorne
+            //       select animal as Licorne;
         }
 
         /// <summary>
@@ -43,7 +52,9 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Animal> LesLicornesSontDesAnimaux()
         {
+            // Version méthode d'extension
             return ListeLicorne.Cast<Animal>();
+            // Pas de version déclarative...
         }
 
         /// <summary>
@@ -52,7 +63,9 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Animal> LesAnimauxQuiNontPasLaChanceDEtreDesLicornes()
         {
+            // Version méthode d'extension
             return ListeAnimaux.Except(ListeLicorne);
+            // Pas de version déclarative...
         }
 
         /// <summary>
@@ -61,7 +74,12 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Licorne> LicorneLesPlusJeuneDabord()
         {
+            // Version méthode d'extension
             return ListeLicorne.OrderBy<Licorne, int>(l => l.Age);
+            // Version déclarative
+            //return from licorne in ListeLicorne
+            //       orderby licorne.Age
+            //       select licorne;
         }
 
         /// <summary>
@@ -70,7 +88,12 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Licorne> LicorneLesPlusJeuneDabordMaislesAAussi()
         {
+            // Version méthode d'extension
             return ListeLicorne.OrderBy<Licorne, int>(l => l.Age).ThenBy(l => l.Name);
+            // Version déclarative
+            //return from licorne in ListeLicorne
+            //       orderby licorne.Age, licorne.Name
+            //       select licorne;
         }
 
         /// <summary>
@@ -79,7 +102,12 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Licorne> CeuxQuiOnt10ansLevezLaMain()
         {
+            // Version méthode d'extension
             return ListeLicorne.Where(l => l.Age == 10);
+            // Version déclarative
+            //return from licorne in ListeLicorne
+            //       where licorne.Age == 10
+            //       select licorne;
         }
 
         /// <summary>
@@ -88,7 +116,9 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<string> LesLicornesOntDuCaractere()
         {
+            // Version méthode d'extension
             return ListeLicorne.SelectMany(l => l.ListePeronnalites);
+            // Pas de version déclarative...
         }
 
         /// <summary>
@@ -97,7 +127,12 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Tuple<int, int>> PyramideDesAgesDesLicornes()
         {
+            // Version méthode d'extension
             return ListeLicorne.GroupBy(l => l.Age, (x, y) => new Tuple<int, int>(x, y.Count()));
+            // Version déclarative
+            //return from licorne in ListeLicorne
+            //       group licorne by licorne.Age into newGroup
+            //       select new Tuple<int, int>(newGroup.Key, newGroup.Count());
         }
 
         /// <summary>
@@ -106,16 +141,23 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public Dictionary<int, List<Licorne>> QuelleLicorneAMonAge()
         {
+            // Version méthode d'extension
             return ListeLicorne.GroupBy(l => l.Age).ToDictionary(x => x.Key, x => x.ToList());
+            // Version déclarative
+            //return (from licorne in ListeLicorne
+            //        group licorne by licorne.Age into newGroup
+            //        select newGroup).ToDictionary(x => x.Key, x => x.ToList());
         }
 
         /// <summary>
         /// Retourner un lookup qui associe à chaque age la ou les licornes associées
         /// </summary>
         /// <returns></returns>
-        public ILookup<int,Licorne> QuelleLicorneAMonAgeLookup()
+        public ILookup<int, Licorne> QuelleLicorneAMonAgeLookup()
         {
+            // Version méthode d'extension
             return ListeLicorne.ToLookup(l => l.Age);
+            // Pas de version déclarative...
         }
 
         /// <summary>
@@ -124,7 +166,12 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public IEnumerable<Tuple<Licorne, List<Jouet>>> AChacunSonJouet()
         {
+            // Version méthode d'extension
             return ListeLicorne.Select(l => new Tuple<Licorne, List<Jouet>>(l, ListeJouet.Where(j => j.NomProprietaire == l.Name).ToList()));
+            // Version déclarative
+            //return from licorne in ListeLicorne
+            //       join jouet in ListeJouet on licorne.Name equals jouet.NomProprietaire into jouetsDeLaLicorne
+            //       select new Tuple<Licorne, List<Jouet>>(licorne, jouetsDeLaLicorne.ToList());
         }
 
         /// <summary>
@@ -134,8 +181,9 @@ namespace ExerciceLinqLicornes
         /// <returns></returns>
         public Licorne MegaLicorne()
         {
+            // Version méthode d'extension
             return ListeLicorne.Aggregate((l1, l2) => Licorne.Fusiiioooon(l1, l2));
-            
+            // Pas de version déclarative...
         }
 
         /// <summary>
@@ -147,7 +195,9 @@ namespace ExerciceLinqLicornes
         /// <remarks>On peut utiliser la méthode Licorne.FaireUnBebe</remarks>
         public IEnumerable<Licorne> DesBebesLicornes()
         {
-            return ListeLicorne.Take(3).Zip(ListeLicorne.Skip(3), (l1,l2) => Licorne.FaireUnBebe(l1,l2));
+            // Version méthode d'extension
+            return ListeLicorne.Take(3).Zip(ListeLicorne.Skip(3), (l1, l2) => Licorne.FaireUnBebe(l1, l2));
+            // Pas de version déclarative...
         }
     }
 }
